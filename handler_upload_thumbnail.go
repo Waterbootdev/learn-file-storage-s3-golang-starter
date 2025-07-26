5a2569dccf7d55524bbe39606b9cabb4325790a5
Waterbootdev <wassermann.sp@gmail.com>
2025-07-26 10:54:46 +0200

    data url

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,12 +1,35 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
 )
+
+const MAX_MEMORY = 10 << 20
+
+func getDataUrl(data []byte, mediaType string) *string {
+	encodedData := base64.StdEncoding.EncodeToString(data)
+	url := fmt.Sprintf("data:%s;base64,%s", mediaType, encodedData)
+	return &url
+}
+
+func getMapUrl(data []byte, mediaType string, port string, videoID uuid.UUID) *string {
+
+	videoThumbnail := thumbnail{
+		data:      data,
+		mediaType: mediaType,
+	}
+
+	videoThumbnails[videoID] = videoThumbnail
+
+	url := fmt.Sprintf("http://localhost:%s/api/thumbnails/{%s}", port, videoID)
+	return &url
+}
 
 func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Request) {
 	videoIDString := r.PathValue("videoID")
@@ -28,10 +51,51 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	err = r.ParseMultipartForm(MAX_MEMORY)
+
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse multipart form", err)
+		return
+	}
+
+	file, header, err := r.FormFile("thumbnail")
+
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+
+	defer file.Close()
+
+	mediaType := header.Header.Get("Content-Type")
+
+	data, err := io.ReadAll(file)
+
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "cant reead file", err)
+		return
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil || video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't get video", err)
+		return
+	}
+
+	video.ThumbnailURL = getDataUrl(data, mediaType)
+
+	//video.ThumbnailURL = getMapUrl(data, mediaType, cfg.port, videoID)
+
+	err = cfg.db.UpdateVideo(video)
+
+	if err != nil || video.UserID != userID {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't update video", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 }
